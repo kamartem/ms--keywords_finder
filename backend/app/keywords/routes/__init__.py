e69f22e1a22b403537f3fe509c0e2a361eebf193
Artem Kamyshansky <ka@media5.com>
2022-12-11 19:28:27 +0300

    typo fix

--- backend/app/keywords/routes/__init__.py
+++ backend/app/keywords/routes/__init__.py
@@ -5,4 +5,4 @@ from app.keywords.routes.html import router as html_router
 
 keywords_router = APIRouter()
 keywords_router.include_router(html_router, prefix="")
-keywords_router.include_router(tasks_router, prefix="/api/tasks", tags=["tesks"])
+keywords_router.include_router(tasks_router, prefix="/api/tasks", tags=["tasks"])
